5d4e038ba1b71342ea628741d0a2680152a7f2f6
Neonomora <abdillahas443@gmail.com>
2025-11-19 07:50:31 +0700

    style test

--- components/HomePageMobile.jsx
+++ components/HomePageMobile.jsx
@@ -34,7 +34,7 @@ function LogoCard() {
           height={50}
           className="rounded-full"
         />
-        <CardTitle className="text-3xl font-light bg-gradient-to-r from-gray-800 to-gray-300 bg-clip-text text-transparent">
+        <CardTitle className="text-3xl font-light from-pink-500 to-violet-500 bg-clip-text text-transparent">
           Sakti Info
         </CardTitle>
       </Link>
